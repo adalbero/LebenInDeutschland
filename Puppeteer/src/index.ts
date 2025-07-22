18df3c210d84662c0ca38336275606669452da53
Adalbero F. Guimaraes <adalbero@gmail.com>
2025-07-22 07:40:21 +0200

    Version 1.14.0

--- Puppeteer/src/index.ts
+++ Puppeteer/src/index.ts
@@ -19,20 +19,24 @@ interface Bundesland {
 }
 
 const bundeslands: Bundesland = {
-    '': '300',
-    'Brandenburg': 'BB',
-    'Berlin': 'BE',
-    'Baden-Württemberg': 'BW',
-    'Bayern': 'BY',
-    'Bremen': 'HB',
-    'Hessen': 'HE',
-    'Hamburg': 'HH',
-    'Mecklenburg-Vorpommern': 'MV',
-    'Niedersachsen': 'NI',
-    'Nordrhein-Westfalen': 'NW',
-    'Rheinland-Pfalz': 'RP',
-    'Schleswig-Holstein': 'SH',
-    'Saarland': 'SL',
+    // '': '300',
+
+    // 'Brandenburg': 'BB',
+    // 'Berlin': 'BE',
+    // 'Baden-Württemberg': 'BW',
+    // 'Bayern': 'BY',
+    // 'Bremen': 'HB',
+
+    // 'Hessen': 'HE',
+    // 'Hamburg': 'HH',
+    // 'Mecklenburg-Vorpommern': 'MV',
+    // 'Niedersachsen': 'NI',
+    // 'Nordrhein-Westfalen': 'NW',
+
+    // 'Rheinland-Pfalz': 'RP',
+    // 'Schleswig-Holstein': 'SH',
+    // 'Saarland': 'SL',
+
     'Sachsen': 'SN',
     'Sachsen-Anhalt': 'ST',
     'Thüringen': 'TH'
